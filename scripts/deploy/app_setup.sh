578ef45cd2931d2bb86e05898dc72a12e9e65366
Anna Helton <ahelton@ingeniumplus.com>
2022-07-07 16:55:30 -0400

    Update app_setup.sh

--- scripts/deploy/app_setup.sh
+++ scripts/deploy/app_setup.sh
@@ -5,7 +5,6 @@ set -e
 cd /var/www/learning-locker-latest
 
 unzip output.zip -d /var/www/learning-locker-latest/
-cp /var/www/learning-locker-latest/health.js /var/www/learning-locker-latest/ui/dist/public/health.js
 
 #run this as nodejs
 su nodejs
