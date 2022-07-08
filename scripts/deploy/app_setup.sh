9ec84d823e0e1176b6d2acd7b3b2feab597dbcd6
Anna Helton <ahelton@ingeniumplus.com>
2022-07-08 14:10:00 -0400

    Update app_setup.sh

--- scripts/deploy/app_setup.sh
+++ scripts/deploy/app_setup.sh
@@ -10,5 +10,9 @@ chown nodejs -R /var/www/learning-locker-latest/
 sudo su nodejs
 cd ~
 . ~/.nvm/nvm.sh
+#double pump the install because it keeps saying not there.
+nvm install 10
+npm install -g yarn
+npm install -g pm2
 
 #may need to add pids folder to project root.
