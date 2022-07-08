5965518d3e22df1a8308a08d99956fea90519da9
Anna Helton <ahelton@ingeniumplus.com>
2022-07-08 14:17:03 -0400

    stupid pm2

--- scripts/deploy/start_server.sh
+++ scripts/deploy/start_server.sh
@@ -4,6 +4,13 @@ set -e
 
 service nginx start
 
+#run this as nodejs
 sudo su nodejs
+cd ~
+. ~/.nvm/nvm.sh
+#double pump the install because it keeps saying not there.
+nvm install 10
+npm install -g yarn
+npm install -g pm2
 cd /var/www/learning-locker-latest
 pm2 start pm2/xapi.json
--- scripts/deploy/stop_server.sh
+++ scripts/deploy/stop_server.sh
@@ -2,6 +2,13 @@
 
 service nginx stop
 
+#run this as nodejs
 sudo su nodejs
+cd ~
+. ~/.nvm/nvm.sh
+#double pump the install because it keeps saying not there.
+nvm install 10
+npm install -g yarn
+npm install -g pm2
 cd /var/www/learning-locker-latest
 pm2 stop pm2/xapi.json
