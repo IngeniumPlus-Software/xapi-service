1e4aff68e652a19e65175c1f842b15f0214f78ce
Anna Helton <ahelton@ingeniumplus.com>
2022-07-08 14:57:53 -0400

    Update stop_server.sh

--- scripts/deploy/stop_server.sh
+++ scripts/deploy/stop_server.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 
 service nginx stop
-
-#run this as nodejs
-sudo su nodejs
-cd ~
-. ~/.nvm/nvm.sh
-#double pump the install because it keeps saying not there.
-nvm install 10
-npm install -g yarn
-npm install -g pm2
-cd /var/www/learning-locker-latest
-#pm2 stop pm2/xapi.json
