777a3f0d4bd00a2f8897fa4253092bd32824aa2c
Anna Helton <ahelton@ingeniumplus.com>
2022-07-21 11:52:13 -0400

    Update stop_server.sh

--- scripts/deploy/stop_server.sh
+++ scripts/deploy/stop_server.sh
@@ -2,8 +2,9 @@
 
 service nginx stop
 #run this as nodejs
-sudo su nodejs
-cd ~
+#sudo su nodejs
+cd /home/nodejs
 # start up nvm so we can use pm2 (npm, yarn...)
-nvm use 10
-pm2 stop pm2/xapi.json
+sudo -u nodejs nvm use 10
+cd /var/www/learning-locker-latest
+sudo -u nodejs stop pm2/xapi.json
