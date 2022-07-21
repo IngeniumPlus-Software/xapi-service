ae237b80270aaa7caacbfe8a9f5c74f186e63f0e
Anna Helton <ahelton@ingeniumplus.com>
2022-07-21 16:33:28 -0400

    trying to fix deployment

--- scripts/deploy/start_pm2.sh
+++ scripts/deploy/start_pm2.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-
+cd ~
 # start up nvm so we can use pm2 (npm, yarn...)
-~/.nvm/nvm.sh use 10
-cd /var/www/learning-locker-latest
-pm2 start pm2/xapi.json
+source ~/.bashrc
+pm2 start /var/www/learning-locker-latest/pm2/xapi.json
--- scripts/deploy/stop_pm2.sh
+++ scripts/deploy/stop_pm2.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # start up nvm so we can use pm2 (npm, yarn...)
-~/.nvm/nvm.sh use 10
+cd ~
+#nvm use 10
+source ~/.bashrc
 cd /var/www/learning-locker-latest
 pm2 stop pm2/xapi.json
