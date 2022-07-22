592d2020588fa3cc286f76bf412b3dccb73fb5a0
Anna Helton <ahelton@ingeniumplus.com>
2022-07-22 08:00:46 -0400

    Update start_pm2.sh

--- scripts/deploy/start_pm2.sh
+++ scripts/deploy/start_pm2.sh
@@ -4,4 +4,5 @@ set -e
 cd ~
 # start up nvm so we can use pm2 (npm, yarn...)
 . ~/.nvm/nvm.sh
-pm2 start /var/www/learning-locker-latest/pm2/xapi.json
+cd /var/www/learning-locker-latest
+pm2 start pm2/xapi.json
