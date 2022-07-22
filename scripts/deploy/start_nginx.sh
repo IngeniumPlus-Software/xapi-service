ba622d559880a1b8751758ea844ab9d0215ebad6
Anna Helton <ahelton@ingeniumplus.com>
2022-07-22 08:31:36 -0400

    Update start_nginx.sh

--- scripts/deploy/start_nginx.sh
+++ scripts/deploy/start_nginx.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
+cd ~
 # Update Nginx conf for environment variables
 # Performing this here instead of postcopy script because the conf file is not present when the postcopy runs.
 sed "s/XAPI_PORT/$EXPRESS_PORT/" /etc/nginx/conf.d/learning-locker.conf > learning-locker.conf & mv -f learning-locker.conf /etc/nginx/conf.d/learning-locker.conf
