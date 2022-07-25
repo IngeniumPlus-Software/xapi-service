4316e6e442b2f84a14e3133e0277892750d2670d
Anna Helton <ahelton@ingeniumplus.com>
2022-07-25 11:55:14 -0400

    Update remove.sh

--- scripts/deploy/remove.sh
+++ scripts/deploy/remove.sh
@@ -4,10 +4,10 @@ set -e
 if [[ -d "/var/www/learning-locker-latest" ]]; then
   cd /var/www/learning-locker-latest
   if [[ -f '/var/www/learning-locker-latest/.env' ]]; then
-    cp .env ~/.env
+    cp /var/www/learning-locker-latest/.env /var/www/.env
   fi
   rm -Rf /var/www/learning-locker-latest
   if [[ -f '~/.env' ]]; then
-    mv ~.env .env
+    mv /var/www/.env /var/www/learning-locker-latest/.env
   fi
 fi
