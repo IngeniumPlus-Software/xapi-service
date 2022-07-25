e1344fb0b5095dc1bf690efc521929366b7956c4
Anna Helton <ahelton@ingeniumplus.com>
2022-07-25 12:16:42 -0400

    handling .env deletion on deploy

--- scripts/deploy/remove.sh
+++ scripts/deploy/remove.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
 set -e
-if [[ -d "/var/www/learning-locker-latest" ]]; then
-  cd /var/www/learning-locker-latest
-  if [[ -f '/var/www/learning-locker-latest/.env' ]]; then
-    cp /var/www/learning-locker-latest/.env /var/www/.env
-  fi
-  rm -Rf /var/www/learning-locker-latest
-  if [[ -f '~/.env' ]]; then
-    mv /var/www/.env /var/www/learning-locker-latest/.env
-  fi
-fi
+
+rm -Rf /var/www/learning-locker-latest
